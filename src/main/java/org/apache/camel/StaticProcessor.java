f0184240331b9f24507b5c6e36393f8f991cb33c
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-01-26 01:50:57 +0200

    Add .gitignore and .idea/vcs.xml files, update StaticProcessor.java and index.html

--- src/main/java/org/apache/camel/StaticProcessor.java
+++ src/main/java/org/apache/camel/StaticProcessor.java
@@ -1,8 +1,5 @@
 package org.apache.camel;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,12 +14,25 @@ public class StaticProcessor implements Processor {
     @Override
     public void process(Exchange exchange) throws Exception {
         String path = exchange.getIn().getHeader(Exchange.HTTP_PATH, String.class);
+
         if (path.startsWith("/")) {
             path = path.substring(1);
         }
+        if (path.isEmpty() || path.endsWith("/")) {
+            path += "index.html";
+        }
+
         Path folderPath = Paths.get(folder, path);
-        if (Files.exists(folderPath)) {
+
+        if (Files.isDirectory(folderPath)) {
+            folderPath = folderPath.resolve("index.html");
+        }
+
+        if (Files.exists(folderPath) && !Files.isDirectory(folderPath)) {
             exchange.getIn().setBody(Files.readString(folderPath));
+        } else {
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, 404);
+            exchange.getIn().setBody("404 Not Found");
         }
     }
 }
