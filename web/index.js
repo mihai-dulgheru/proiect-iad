45219e6908f53ae1cb63f56ad79202fb8266aa15
Claudiu <claudeady4579@gmail.com>
2024-02-03 02:08:59 +0200

    te rog nu judeca codul, o sa il fac mai frumos

--- web/index.js
+++ web/index.js
@@ -1,6 +1,198 @@
 const apiEndpoint = "/api/players";
 const results = document.getElementById("results");
 
+const DUMMY_DATA = [
+  {
+    firstname: "Magnus",
+    lastname: "Carlsen",
+    club: "World Chess Club",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Vishy",
+    lastname: "Anand",
+    club: "International Chess Association",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Judith",
+    lastname: "Polgar",
+    club: "Hungarian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Hou",
+    lastname: "Yifan",
+    club: "Chinese Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Levon",
+    lastname: "Aronian",
+    club: "Armenian Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Nona",
+    lastname: "Gaprindashvili",
+    club: "Georgian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Garry",
+    lastname: "Kasparov",
+    club: "Russian Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Judit",
+    lastname: "Polgar",
+    club: "Hungarian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Viswanathan",
+    lastname: "Anand",
+    club: "International Chess Association",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Maia",
+    lastname: "Chiburdanidze",
+    club: "Georgian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Bobby",
+    lastname: "Fischer",
+    club: "United States Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Pia",
+    lastname: "Cramling",
+    club: "Swedish Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Fabiano",
+    lastname: "Caruana",
+    club: "United States Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Nigel",
+    lastname: "Short",
+    club: "English Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Antoaneta",
+    lastname: "Stefanova",
+    club: "Bulgarian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Mikhail",
+    lastname: "Tal",
+    club: "Latvian Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Nana",
+    lastname: "Dzagnidze",
+    club: "Georgian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Alexander",
+    lastname: "Alekhine",
+    club: "Russian Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Kateryna",
+    lastname: "Lagno",
+    club: "Russian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Maxime",
+    lastname: "Vachier-Lagrave",
+    club: "French Chess Federation",
+    title: "Grandmaster",
+    gender: "Male",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+  {
+    firstname: "Judit",
+    lastname: "Polgar",
+    club: "Hungarian Chess Federation",
+    title: "Grandmaster",
+    gender: "Female",
+    status: "Active",
+    avatar: "/images/player.svg",
+  },
+];
+
 async function fetchAndDisplayPlayers(filterType, filterValue) {
   results.innerHTML = "Searching for players...";
 
@@ -12,6 +204,8 @@ async function fetchAndDisplayPlayers(filterType, filterValue) {
         "X-Filter-Value": filterValue,
       },
     });
+
+    // comment this line for testing and add DUMMY_DATA in the displayPlayers function
     const players = await response.json();
 
     displayPlayers(players);
@@ -42,6 +236,7 @@ function filterPlayers() {
 }
 
 function displayPlayers(players) {
+  console.log(players);
   results.innerHTML = players.length > 0 ? "" : "No players found.";
   const downloadButton = document.getElementById("download-results");
   downloadButton.classList.toggle("cursor-not-allowed", players.length === 0);
@@ -49,7 +244,8 @@ function displayPlayers(players) {
 
   players.forEach((player) => {
     const playerElement = document.createElement("div");
-    playerElement.className = "p-4 mb-2 bg-white rounded shadow cursor-pointer";
+    playerElement.className =
+      "pt-4 mb-2 bg-transparent rounded-xl shadow cursor-pointer border border-1.5 border-[#BE995E] ";
     playerElement.innerHTML = getPlayerHTML(player);
 
     playerElement.addEventListener("click", () => {
@@ -62,11 +258,18 @@ function displayPlayers(players) {
 
 function getPlayerHTML(player) {
   return `
-    <p><strong>Name:</strong> ${player.firstname} ${player.lastname}</p>
-    <p><strong>Club:</strong> ${player.club}</p>
-    <p><strong>Title:</strong> ${player.title}</p>
-    <p><strong>Gender:</strong> ${player.gender}</p>
-    <p><strong>Status:</strong> ${player.status}</p>
+    <div>
+      <div class='h-44 flex justify-center'>
+        <img src="${player.avatar}" alt="" class='h-full'>
+      </div>
+      <div class="bg-gradient-to-r from-[#8E6233] via-[#D3B589] to-[#BE995E] text-black pl-3 py-2">
+      <p><strong>Name:</strong> ${player.firstname} ${player.lastname}</p>
+      <p><strong>Club:</strong> ${player.club}</p>
+      <p><strong>Title:</strong> ${player.title}</p>
+      <p><strong>Gender:</strong> ${player.gender}</p>
+      <p><strong>Status:</strong> ${player.status}</p>
+      </div>
+    </div>
     <!-- Add more player details here -->
   `;
 }
