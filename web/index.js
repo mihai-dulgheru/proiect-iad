bc2957c1c0b52b2a097d6e8540942ca11b380393
Mihai-Nicolae Dulgheru <dulgherumihai19@stud.ase.ro>
2024-02-04 00:30:21 +0200

    Refactor playerElement class in displayPlayers function

--- web/index.js
+++ web/index.js
@@ -79,7 +79,7 @@ function displayPlayers(players) {
   players.forEach((player) => {
     const playerElement = document.createElement("div");
     playerElement.className =
-      "pt-4 mb-2 bg-transparent rounded-xl shadow cursor-pointer border border-1.5 border-[#BE995E] ";
+      "pt-4 mb-2 bg-transparent rounded-xl shadow cursor-pointer border border-1.5 border-[#BE995E]";
     playerElement.innerHTML = getPlayerHTML(player);
 
     playerElement.addEventListener("click", () => {
@@ -93,15 +93,21 @@ function displayPlayers(players) {
 function getPlayerHTML(player) {
   return `
     <div>
-      <div class='h-44 flex justify-center'>
-        <img src="${player.avatar}" alt="" class='h-full'>
+      <div class="h-44 flex justify-center">
+        <img
+          src="${player.avatar}"
+          alt="${player.firstname} ${player.lastname}"
+          class="h-full"
+        />
       </div>
-      <div class="bg-gradient-to-r from-[#8E6233] via-[#D3B589] to-[#BE995E] text-black pl-3 py-2 rounded-b-lg">
-      <p><strong>Name:</strong> ${player.firstname} ${player.lastname}</p>
-      <p><strong>Club:</strong> ${player.club}</p>
-      <p><strong>Title:</strong> ${player.title}</p>
-      <p><strong>Gender:</strong> ${player.gender}</p>
-      <p><strong>Status:</strong> ${player.status}</p>
+      <div
+        class="bg-gradient-to-r from-[#8E6233] via-[#D3B589] to-[#BE995E] text-black pl-3 py-2 rounded-b-lg"
+      >
+        <p><strong>Name:</strong> ${player.firstname} ${player.lastname}</p>
+        <p><strong>Club:</strong> ${player.club}</p>
+        <p><strong>Title:</strong> ${player.title}</p>
+        <p><strong>Gender:</strong> ${player.gender}</p>
+        <p><strong>Status:</strong> ${player.status}</p>
       </div>
     </div>
     <!-- Add more player details here -->
